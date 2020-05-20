a861fb14a32215788b1dad6180eb76278d0b8691
success <chizaramsuccess9@gmail.com>
2020-05-20 12:31:13 +0100

    news_app

--- lib/screens/news_description.dart
+++ lib/screens/news_description.dart
@@ -1,14 +1,33 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/text_to_speech/player.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class NewsDescription extends StatelessWidget {
+class NewsDescription extends StatefulWidget {
   final journal;
 
   NewsDescription({this.journal});
 
   @override
+  _NewsDescriptionState createState() => _NewsDescriptionState();
+}
+
+class _NewsDescriptionState extends State<NewsDescription> {
+  final FlutterTts flutterTts = FlutterTts();
+  bool isPlaying;
+
+  @override
   Widget build(BuildContext context) {
+    Future _speak() async {
+      await flutterTts.setLanguage("en-US");
+      await flutterTts.setPitch(1);
+      await flutterTts.speak(widget.journal.content);
+      setState(() {
+        isPlaying = false;
+      });
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -18,21 +37,15 @@ class NewsDescription extends StatelessWidget {
             Stack(
               children: <Widget>[
                 Container(
-                  height: MediaQuery
-                      .of(context)
-                      .size
-                      .height / 2.6,
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width,
+                  height: MediaQuery.of(context).size.height / 2.6,
+                  width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(5),
                       bottomRight: Radius.circular(5),
                     ),
                     image: DecorationImage(
-                        image: NetworkImage(journal.urlToImage),
+                        image: NetworkImage(widget.journal.urlToImage),
                         fit: BoxFit.cover),
                   ),
                 ),
@@ -57,7 +70,7 @@ class NewsDescription extends StatelessWidget {
                   children: <Widget>[
                     Padding(
                       padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 10),
-                      child: Text(journal.title,
+                      child: Text(widget.journal.title,
                           style: TextStyle(
                               fontWeight: FontWeight.bold, fontSize: 20)),
                     ),
@@ -67,7 +80,7 @@ class NewsDescription extends StatelessWidget {
                     Padding(
                       padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 10),
                       child: Text(
-                        journal.content,
+                        widget.journal.content,
                         style: TextStyle(
                             fontSize: 16, fontWeight: FontWeight.w600),
                       ),
@@ -75,7 +88,35 @@ class NewsDescription extends StatelessWidget {
                     SizedBox(
                       height: 5,
                     ),
-                  //  PlayerWidget(body: journal.content),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        IconButton(
+                            icon: Icon(Icons.play_arrow),
+                            iconSize: 30,
+                            onPressed: () => _speak()),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 15.0),
+                          child: TyperAnimatedTextKit(
+                              onTap: () {
+                              },
+                              text: [
+                                "Tap The Button To Speak",
+                              ],
+                              textStyle: TextStyle(
+                                  fontSize: 15.0,
+                                  fontFamily: "Bobbers"
+                              ),
+                              textAlign: TextAlign.center,
+                              alignment: AlignmentDirectional.center // or Alignment.topLeft
+                          ),
+                        ),
+                      ],
+                    ),
                     Column(
                       children: <Widget>[
                         Padding(
@@ -93,7 +134,7 @@ class NewsDescription extends StatelessWidget {
                                 launchUrl();
                               },
                               child: Text(
-                                journal.url,
+                                widget.journal.url,
                                 style: TextStyle(
                                     color: Colors.blue,
                                     fontStyle: FontStyle.italic),
@@ -112,7 +153,7 @@ class NewsDescription extends StatelessWidget {
   }
 
   launchUrl() async {
-    String url = journal.url;
+    String url = widget.journal.url;
     if (await canLaunch(url)) {
       await launch(url);
     } else {
